8ecce32e4468737c60fa00ffc6c6e7fcc824aac9
Todd Anderson <tanderson@launchdarkly.com>
2023-05-22 15:59:19 -0500

    Updating hello-android to init LDClient in Application creation instead of UI layer.

--- app/src/main/java/com/launchdarkly/hello_android/MainActivity.java
+++ app/src/main/java/com/launchdarkly/hello_android/MainActivity.java
@@ -1,39 +1,74 @@
 package com.launchdarkly.hello_android;
 
+import static com.launchdarkly.hello_android.MainApplication.LAUNCHDARKLY_MOBILE_KEY;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.launchdarkly.sdk.LDUser;
 import com.launchdarkly.sdk.android.LDClient;
-import com.launchdarkly.sdk.android.LDConfig;
+import com.launchdarkly.sdk.android.LaunchDarklyException;
 
 // Set FLAG_KEY to the feature flag key you want to evaluate.
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "LDMainActivity";
-    private static final String FLAG_KEY = "my-boolean-flag";
+
+    private static final String TAG = "MainActivity";
+
+    // Set BOOLEAN_FLAG_KEY to the feature flag key you want to evaluate.
+    private static final String BOOLEAN_FLAG_KEY = "my-boolean-flag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textview);
 
-// Set mobileKey to your LaunchDarkly mobile key.
-        LDConfig ldConfig = new LDConfig.Builder()
-                .mobileKey("MOBILE_KEY")
-                .build();
+        if (BOOLEAN_FLAG_KEY == "my-boolean-flag") {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("BOOLEAN_FLAG_KEY was not customized for your account.");
+            builder.create().show();
+        }
 
-// Set up the user properties. This user should appear on your LaunchDarkly users dashboard soon after you run the demo.
-        LDUser user = new LDUser.Builder("example-user-key")
-                .name("Sandy")
-                .build();
+        if (LAUNCHDARKLY_MOBILE_KEY == "mobile-key-from-launch-darkly-website") {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("LAUNCHDARKLY_MOBILE_KEY was not customized for your account.");
+            builder.create().show();
+        }
 
-        LDClient client = LDClient.init(getApplication(), ldConfig, user, 5);
+        LDClient  client;
+        try {
+            client = LDClient.get();
+        } catch (LaunchDarklyException e) {
+            Log.e(TAG, "Unexpected error getting client.", e);
+            return;
+        }
 
-        boolean flagValue = client.boolVariation(FLAG_KEY, false);
-        Log.i(TAG, String.format("Feature flag [%s] is [%s] for this user", FLAG_KEY, flagValue));
+        // to get the variation the SDK has cached
+        textView.setText(
+                getString(
+                        R.string.flag_evaluated,
+                        BOOLEAN_FLAG_KEY,
+                        Boolean.toString(client.boolVariation(BOOLEAN_FLAG_KEY, false))
+                )
+        );
 
+        // to register a listener to get updates in real time
+        client.registerFeatureFlagListener(BOOLEAN_FLAG_KEY, flagKey -> {
+            textView.setText(
+                    getString(
+                            R.string.flag_evaluated,
+                            BOOLEAN_FLAG_KEY,
+                            Boolean.toString(client.boolVariation(BOOLEAN_FLAG_KEY, false))
+                    )
+            );
+        });
+
+        // This call is just to make sure all evaluation events show up immediately for this demo.
+        // Otherwise they will be sent at some point in the future.  You don't need to call this
+        // in production, it is handled automatically at a interval (it is customizable).
         client.flush();
     }
 }
